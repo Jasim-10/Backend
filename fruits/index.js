1918614225c3edd6a4aa31ba6846f29d67683de9
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-26 13:05:11 +0530

    Backend

---
+++ fruits/index.js
@@ -0,0 +1,10 @@
+const apple = require("./apple");
+const banana = require("./banana");
+const orange = require("./mango");
+
+let fruits = [apple,banana,orange];
+
+module.exports = fruits;
+
+
+
---
+++ script.js
@@ -0,0 +1,8 @@
+const fruits = require("./fruits");
+// const math = require("./math");
+
+// console.log(math.sum(2,8));
+
+console.log(fruits);
+
+
